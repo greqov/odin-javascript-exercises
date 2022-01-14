3b9de033e1df8f1653f501ec8ef2afb74cf2a750
Ivan Greqov <idgreq@bk.ru>
2022-01-14 15:46:21 +0300

    Complete 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (ftemp) {
+  const ctemp = ((ftemp - 32) * 5) / 9;
+  return Number(ctemp.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (ctemp) {
+  const ftemp = (ctemp * 9) / 5 + 32;
+  return Number(ftemp.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
