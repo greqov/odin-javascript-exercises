67227a9c7ba8d5e2c3e0c147cb50f7e53c4e2167
Ivan Greqov <idgreq@bk.ru>
2022-01-14 15:19:17 +0300

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function () {
+  let args = [...arguments];
+  let sum = 0;
 
+  for (let el of args) {
+    if (el < 0 || typeof el !== 'number') return 'ERROR';
+  }
+
+  args = args.sort((a, b) => a - b);
+
+  for (let i = args[0]; i <= args[1]; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
