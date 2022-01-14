e71b063f76d9be1311b99d37cebaba5d96d8a898
Ivan Greqov <idgreq@bk.ru>
2022-01-14 14:36:00 +0300

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  let [arr, ...targets] = [...arguments];
 
+  targets.forEach((el) => {
+    arr = arr.filter((arr_el) => arr_el !== el);
+  });
+
+  return arr;
 };
 
 // Do not edit below this line
