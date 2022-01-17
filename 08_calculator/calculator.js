d42c813c472b1e87e879027569e4591b2f0dcd53
Ivan Greqov <idgreq@bk.ru>
2022-01-17 20:13:58 +0300

    Complete 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function () {
+  const [arr] = [...arguments];
+
+  if (arr.length === 0) return 0;
+
+  return arr.reduce((prev, curr) => {
+    return curr + prev;
+  });
 };
 
-const multiply = function() {
+const multiply = function () {
+  const [arr] = [...arguments];
 
+  return arr.reduce((prev, curr) => {
+    return curr * prev;
+  });
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) return 1;
+
+  let res = 1;
+
+  for (let i = 1; i <= num; i++) {
+    res *= i;
+  }
+
+  return res;
 };
 
 // Do not edit below this line
@@ -29,5 +47,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
