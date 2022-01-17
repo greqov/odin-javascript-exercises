37ae4b5d3bee277daed60c30dad142ff320e0ef1
Ivan Greqov <idgreq@bk.ru>
2022-01-18 00:51:38 +0300

    Complete 10_fibonacci exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 1) return 'OOPS';
 
+  let arr = [1, 1];
+
+  for (let i = 2; i < num; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+
+  return arr[num - 1];
 };
 
 // Do not edit below this line
