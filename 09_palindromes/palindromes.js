31271476b0d310a71cfa1e5e827a161ea354cb1d
Ivan Greqov <idgreq@bk.ru>
2022-01-17 20:33:56 +0300

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let str = string;
+  const punctuation = [' ', ',', '!', '.'];
+  let formattedStr = str
+    .toLowerCase()
+    .split('')
+    .filter((i) => {
+      return !punctuation.includes(i);
+    })
+    .join('');
 
+  return formattedStr === formattedStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
