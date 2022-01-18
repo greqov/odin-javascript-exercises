fff5c7529dc124c24efe094b83b69f07c2dadb2a
Ivan Greqov <idgreq@bk.ru>
2022-01-18 13:33:34 +0300

    Complete 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function () {
+  const [people] = [...arguments];
 
+  // get lifetime arr
+  const lifetime = people.map((user) => {
+    const currentYear = new Date().getFullYear();
+    const yearOfDeath = user.yearOfDeath || currentYear;
+    return yearOfDeath - user.yearOfBirth;
+  });
+
+  // get index of the max lifetime
+  const max = Math.max(...lifetime);
+  const index = lifetime.findIndex((i) => i === max);
+
+  // return user obj with index
+  return people[index];
 };
 
 // Do not edit below this line
